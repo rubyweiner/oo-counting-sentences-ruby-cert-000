59d1d75275f6fe79edc3366a8a5ddbc6344a289a
Ruby Weiner <rubyweiner796@me.com>
2017-09-06 03:05:55 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
+
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
 
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size
   end
+
 end
